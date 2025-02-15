6c8d4e94307c64232ecadf2bf05908b9ed7be1c3
Kevin Kam <kevinkamzw@gmail.com>
2025-02-15 18:02:34 +0800

    changed port to 8080

--- application.py
+++ application.py
@@ -52,4 +52,4 @@ def predict_math_score():
         except Exception as e:
             raise MLException(e, sys)
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=5000)
+    application.run(host='0.0.0.0', port=8080)
